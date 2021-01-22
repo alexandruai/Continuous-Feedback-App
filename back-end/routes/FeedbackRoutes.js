b73ed183dec7f39c9216fb33559d28a8852c23b9
Alex <masala.alex13@gmail.com>
2021-01-22 15:49:12 +0200

    updated feedback routes

--- back-end/routes/FeedbackRoutes.js
+++ back-end/routes/FeedbackRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createFeedback } from '../logic/FeedbackLogic.js';
+import { createFeedback, getFeedbackById, getFeedbacks, updateFeedback } from '../logic/FeedbackLogic.js';
 
 const router = express.Router();
 
@@ -7,5 +7,17 @@ router.route('/createFeedback/:id').post(async (req, res) => {
     return res.status(201).json(await createFeedback(req.params.id,req.body));
     });
     
+router.route('/feedbacks').get(async (req, res) => {
+return res.json(await getFeedbacks());
+});
+router.route('/feedback/:id').get(async (req, res) => {
+return res.json(await getFeedbackById(req.params.id));
+});
+router.route('/updateFeedback/:id').put(async (req, res) => {
+return res.json(await updateFeedback(req.params.id, req.body));
+});
+router.route('/deleteFeedback/:id').delete(async (req, res) => {
+return res.json(await deleteFeedback(req.params.id));
+});
 
-  export default router;
+export default router;
